8ee9b36aa0008ab6b9ef74f9114254d56e6ba429
DaviLu1566 <davi.mariano1566@gmail.com>
2026-03-27 22:18:14 -0300

    Add files via upload

---
+++ Python/ex21.py
@@ -0,0 +1,20 @@
+D1= int(input("Digite um número "))
+D2= int(input("Digite outro número "))
+Adição= (D1+D2)
+Subtração= (D1-D2)
+Multiplicação= (D1*D2)
+Divisão= (D1/D2)
+receba= input("digite o tipo de calculo desejado ? ")
+if Adição:
+    print(Adição)
+elif Subtração:
+    print(Subtração)
+elif Multiplicação:
+    print(Multiplicação)
+elif Divisão:
+    print(Divisão)
+else: 
+    print("Tipo de calculo incorreto, tente algo como: Adição, Subtração, Multiplicação e Divisão") 
+                       
+
+
