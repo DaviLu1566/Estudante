c9814c896042c07ab61b494e6ce0a060423073d1
DaviLu1566 <davi.mariano1566@gmail.com>
2026-05-15 01:03:38 -0300

    Update transformar.js

--- Html/Avaliação/transformar.js
+++ Html/Avaliação/transformar.js
@@ -20,7 +20,14 @@ function sc_davi_transformar() {
     const sc_davi_perfil = document.querySelector(".sc_davi_perfil");
     const sc_davi_descricao_box = document.querySelector(".sc_davi_descricao");
 
-    if (!sc_davi_transformado) {
+   if (!sc_davi_transformado) {
+
+    /* FLASH + TREMOR */
+    document.body.classList.add("flash_transformacao");
+    document.body.classList.add("tela_tremendo");
+
+    /* ESPERA O EFEITO */
+    setTimeout(() => {
 
         /*
         TRANSFORMA EM KAMEN RIDER BLACK RX
@@ -28,7 +35,7 @@ function sc_davi_transformar() {
 
         sc_davi_nome.textContent = "Kamen Rider Black RX";
 
-        sc_davi_foto.src = "img/kamen-rider-rx.webp";
+        sc_davi_foto.src = "kamen-rider-rx.webp";
 
         sc_davi_lista.innerHTML = `
             <li>Nome: Kamen Rider Black RX</li>
@@ -37,17 +44,23 @@ function sc_davi_transformar() {
         `;
 
         sc_davi_descricao.textContent =
-            "Kamen Rider Black RX é a evolução do herói Kamen Rider Black. Após ser lançado no espaço pelo império Crisis, Kotaro Minami recebe o poder do Sol e se torna o RX. Seu objetivo é proteger a Terra e derrotar o mal.";
+            "Kamen Rider Black RX é a evolução do herói Kamen Rider Black. Após ser lançado no espaço pelo império Crisis, Davi Lupette recebe o poder do Sol e se torna o RX. Seu objetivo é proteger a Terra e derrotar o mal.";
 
         sc_davi_btn.textContent = "Voltar!";
 
-        /* 🔥 ATIVA CORES RX */
+        /*  ATIVA CORES RX */
         document.body.classList.add("rx_ativo");
         sc_davi_perfil.classList.add("rx_ativo");
         sc_davi_descricao_box.classList.add("rx_ativo");
         sc_davi_btn.classList.add("rx_ativo");
 
         sc_davi_transformado = true;
+
+        /* REMOVE EFEITOS */
+        document.body.classList.remove("flash_transformacao");
+        document.body.classList.remove("tela_tremendo");
+
+    }, 500);
 
     } else {
 
@@ -57,7 +70,7 @@ function sc_davi_transformar() {
 
         sc_davi_nome.textContent = "Quem sou eu ?";
 
-        sc_davi_foto.src = "img/Davi-Lupette.jpg";
+        sc_davi_foto.src = "Davi-Lupette.jpg";
 
         sc_davi_lista.innerHTML = `
             <li>Nome: Davi Lupete Trigueiro Mariano.</li>
@@ -68,9 +81,9 @@ function sc_davi_transformar() {
         sc_davi_descricao.textContent =
             "Olá, sou o Davi, sou um estudante de Análise e Desenvolvimento de Sistemas na faculdade de ciências médicas de São Paulo.";
 
-        sc_davi_btn.textContent = "Transformar!";
+        sc_davi_btn.textContent = "Henshin!";
 
-        /* 🔄 REMOVE CORES RX */
+        /*  REMOVE CORES RX */
         document.body.classList.remove("rx_ativo");
         sc_davi_perfil.classList.remove("rx_ativo");
         sc_davi_descricao_box.classList.remove("rx_ativo");
