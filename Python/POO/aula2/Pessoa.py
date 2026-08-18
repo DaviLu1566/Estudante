b7f6c90808f9fe2adb877728c660a04bcf253468
DaviLu1566 <davi.mariano1566@gmail.com>
2026-08-17 22:24:45 -0300

    Add files via upload

---
+++ Python/POO/aula2/Pessoa.py
@@ -0,0 +1,9 @@
+#class é para criar casses para começar a fazer o objeto
+class Pessoa:
+    def __init__(self, nome, idade, cpf, email):
+        self.nome = nome 
+        self.idade = idade 
+        self.cpf = cpf
+        self.email = email
+
+
